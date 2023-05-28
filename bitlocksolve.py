58b7cefd1c3d779ab94171418d5ec6e33958d6f1
mhastu <hasenhuetl@student.tugraz.at>
2023-05-28 09:35:04 +0200

    restructure

--- bitlocksolve.py
+++ bitlocksolve.py
@@ -1,21 +1,41 @@
 #!/bin/python3
 import sys
 from solver import Solver
+import argparse
 
-if len(sys.argv) < 2:
-    print("usage:", sys.argv[0], " <mapfilename> [-w|-W <arrows>]")
-    print("      -w:          walkthrough after completion")
-    print("      -W <arrows>: walkthrough given direction arrows")
-    exit(1)
-filename = sys.argv[1]
-solver = Solver(filename)
-if (len(sys.argv) > 2) and (sys.argv[2] == "-W"):
-    if len(sys.argv) < 4:
-        print("missing positional argument for -W: arrows")
-        exit(2)
-    solver.walkthrough(sys.argv[3])
-    exit(0)
 
-path = solver.solve()
-if (len(sys.argv) > 2) and (sys.argv[2] == "-w"):
-    solver.walkthrough(path)
+class BitLockSolve:
+    def __init__(self):
+        self.parse_args()
+
+    def main(self):
+        solver = Solver(self.args.mapfilename)
+
+        if self.args.walkthrough_path:  # just simulate walkthrough, do not solve
+            solver.walkthrough(sys.argv[3])
+            exit(0)
+
+        path = solver.solve()
+        if self.args.walkthrough:
+            solver.walkthrough(path)
+
+    def parse_args(self):
+        argparser = argparse.ArgumentParser(
+            prog="B.i.t.Lock Solver",
+            description="""Solve levels by brute-force.
+            If walkthrough is simulated, press enter to advance to next step.
+            """
+        )
+        argparser.add_argument("mapfilename")
+        argparser.add_argument("-w", "--walkthrough", action="store_true",
+                            help="Walkthrough after completion")
+        argparser.add_argument("-W", "--walkthrough-path", type=str, metavar="arrows",
+                            help="Walk through given direction arrows (do not solve)")
+
+        self.args = argparser.parse_args()
+
+
+# ========== ENTRY POINT ==========
+if __name__ == "__main__":
+    BitLockSolve().main()
+# =================================
